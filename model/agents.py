e486eae69d3b2873a421a5135c3b27ce8b6cbf25
shane <shane.dicks1@gmail.com>
2022-02-09 14:06:44 -0800

    feature_interactions represented as network, plus several output generating functions added to World

--- model/agents.py
+++ model/agents.py
@@ -15,96 +15,14 @@ def name_from_number(num: int, lower: bool = True):
     return ''.join(reversed(letters))
 
 
-class Agent(Agent):
-    def __init__(
-        self, 
-        unique_id: int, 
-        model: "World", 
-        traits: Dict["Feature", str],
-        utils: float,
-        trait_mutation_chance: float = 0.02,
-        new_trait_chance: float = 0.002
-    ) -> None:
-        super().__init__(unique_id, model)
-        self.utils = utils
-        self.traits = traits
-        self.trait_mutation_chance = trait_mutation_chance
-        self.new_trait_chance = new_trait_chance
-
-    def get_interactions(self) -> List["Interaction"]:
-        interactions = self.model.interactions.values()
-        features = [f for f in self.traits.keys()]
-        return [
-            x
-            for x 
-            in interactions 
-            if x.initiator in features
-        ]
-
-    def interact(self) -> None:
-        interactions = self.get_interactions()
-        for i in interactions:
-            i.do_interaction(init_agent=self)
-            print(self, self.utils)
-
-    def reproduce(self) -> None:
-        model = self.model
-        new_agent = Agent(
-            unique_id = model.next_id(),
-            model = model,
-            utils = model.base_utils,
-            traits = self.traits,
-        )
-        model.schedule.add(new_agent)
-        if model.random.random() <= self.trait_mutation_chance:
-            feature = model.random.choice(list(new_agent.traits))
-            new_values = [
-                x
-                for x
-                in feature.values
-                if x != new_agent.traits[feature]
-            ]
-            new_agent.traits[feature] = model.random.choice(new_values)
-        if model.random.random() <= self.new_trait_chance:
-            features = [
-                f
-                for f
-                in model.features.values()
-                if f.env is False and f not in new_agent.features
-            ]
-            if len(features) > 0:
-                feature = model.random.choice(features)
-            else:
-                feature = model.create_feature()
-            new_agent.traits[feature] = model.random.choice(feature)
-        return
-
-    def die(self) -> None:
-        self.model.schedule.remove(self)
-
-    def step(self):
-        if self.utils >= 0:
-            self.interact()
-            if self.utils > len(self.traits):
-                self.reproduce()
-        else:
-            self.die()
-
-
-    def __repr__(self) -> str:
-        return "Agent {}".format(self.unique_id)
-
-
 class Feature:
 
     def __init__(
         self,
-        model: "World",
-        feature_id: int, 
+        feature_id: int,
         num_values: int,
         env: bool,
     ) -> None:
-        self.model = model
         self.name = name_from_number(feature_id, lower=False)
         self.env = env
         self.values = []
@@ -113,9 +31,7 @@ class Feature:
             self.values.append(value)
 
     def __repr__(self) -> str:
-        return "Feature {0}".format(
-            self.name, 
-        )
+        return self.name
 
 
 class Interaction:
@@ -123,36 +39,38 @@ class Interaction:
     def __init__(
         self,
         model: "World",
-        initiator: Feature, 
-        target: Feature, 
-        weight: float = 1.0,
+        initiator: Feature,
+        target: Feature,
         payoffs: Dict[str, Dict[str, Tuple[float, float]]] = None
     ) -> None:
         self.model = model
         self.random = model.random
         self.initiator = initiator
         self.target = target
-        self.weight = weight
         if payoffs is None:
             self.payoffs = {}
             for i_value in self.initiator.values:
                 self.payoffs[i_value] = {}
                 for t_value in self.target.values:
-                    i, t = self.random.uniform(-1, 1), self.random.uniform(-1, 1)
+                    i, t = round(self.random.uniform(-1, 1), 2), round(self.random.uniform(-1, 1), 2)
                     self.payoffs[i_value][t_value] = (i, t)
         else:
-            self.payoffs = payoffss
+            self.payoffs = payoffs
 
-    def get_interactor(self, feature: Feature, agent: Agent = None) -> Agent:
+    def get_interactor(
+        self,
+        feature: Feature,
+        agent: Agent = None
+    ) -> Agent:
         agents = self.model.schedule.agents
         if agent is None:
-            choices = [x for x in agents if feature in x.traits.keys()]
+            choices = [x for x in agents if feature in x.traits]
         else:
             choices = [
                 x
-                for x 
+                for x
                 in agents
-                if x is not agent and feature in x.traits.keys() 
+                if x is not agent and feature in x.traits
         ]
         return self.random.choice(choices)
 
@@ -167,7 +85,7 @@ class Interaction:
             init_agent = init_agent
         if target_agent is None:
             target_agent = self.get_interactor(
-                feature = self.target, 
+                feature = self.target,
                 agent = init_agent
             )
         else:
@@ -196,7 +114,91 @@ class Interaction:
             self.agent_interaction(init_agent, target_agent)
 
     def __repr__(self) -> str:
-        return "Interaction {0}->{1}".format(
+        return "{0}>{1}".format(
             self.initiator.name, 
             self.target.name
         )
+
+
+class Agent(Agent):
+    def __init__(
+        self,
+        unique_id: int,
+        model: "World",
+        traits: Dict["Feature", str],
+        utils: float,
+        trait_mutation_chance: float = 0.1,
+        new_trait_chance: float = 0.02
+    ) -> None:
+        super().__init__(unique_id, model)
+        self.utils = utils
+        self.traits = traits
+        self.trait_mutation_chance = trait_mutation_chance
+        self.new_trait_chance = new_trait_chance
+        self.interactions = self.get_interactions()
+        self.age = 0
+
+    def get_interactions(self) -> List["Interaction"]:
+        features = [f for f in self.traits.keys()]
+        return [
+            x[2]
+            for x
+            in self.model.feature_interactions.edges(nbunch=features, data='object')
+        ]
+
+    def interact(self) -> None:
+        for i in self.interactions:
+            i.do_interaction(init_agent=self)
+
+    def reproduce(self) -> None:
+        model = self.model
+        new_traits = self.traits.copy()
+        for feature in new_traits:
+            if model.random.random() <= self.trait_mutation_chance:
+                new_values = [
+                    x
+                    for x
+                    in feature.values
+                    if x != new_traits[feature]
+                ]
+                new_traits[feature] = model.random.choice(new_values)
+                print("Trait Mutation in {0}: {1}->{2}".format(feature, self.traits[feature], new_traits[feature]))
+        if model.random.random() <= self.new_trait_chance:
+            features = [
+                f
+                for f
+                in model.feature_interactions.nodes
+                if f.env is False and f not in new_traits
+            ]
+            features.append('new')
+            feature = model.random.choice(features)
+            if feature == 'new':
+                feature = model.create_feature()
+                print("Feature aquired - new feature {}".format(feature))
+            else:
+                print("Feature aquired- existing feature {}".format(feature))
+            new_traits[feature] = model.random.choice(feature.values)
+        new_agent = Agent(
+            unique_id = model.next_id(),
+            model = model,
+            utils = model.base_utils,
+            traits = new_traits,
+        )
+        #print("Reproduction! Welcome {new_agent}".format(new_agent=new_agent.unique_id))
+        model.schedule.add(new_agent)
+        return
+
+    def die(self) -> None:
+        self.model.schedule.remove(self)
+
+    def step(self):
+        if self.utils >= 0:
+            self.interact()
+            if self.utils > len(self.traits):
+                self.reproduce()
+            self.age += 1
+        else:
+            self.die()
+
+    def __repr__(self) -> str:
+        return "Agent {}".format(self.unique_id)
--- model/model.py
+++ model/model.py
@@ -1,46 +1,43 @@
 from typing import Dict, List, Tuple
 import random
-
+import networkx as nx
+import matplotlib.pyplot as plt
 from mesa import Model
 from mesa.datacollection import DataCollector
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 
-from .agents import Agent, Feature, Interaction
+from agents import Agent, Feature, Interaction
 
 
 class World(Model):
 
     def __init__(
         self,
-        features: Dict[int, Feature] = None,
-        interactions: Dict[int, Interaction] = None,
-        num_env_features: int = 1,
-        num_agent_features: int = 2,
-        num_feature_interactions: int = 2,
+        feature_interactions: nx.digraph.DiGraph = None,
+        num_env_features: int = 2,
+        num_agent_features: int = 4,
+        num_feature_interactions: int = 5,
         initial_agents: int = 100,
         num_agent_traits: int = 2,
         base_utils: float = 0.0
     ) -> None:
         super().__init__()
-        self.features = features
-        self.interactions = interactions
+        self.feature_interactions = feature_interactions
         self.base_utils = base_utils
         self.num_agent_traits = num_agent_traits
         self.num_feature_interactions = num_feature_interactions
         self.schedule = RandomActivation(self)
         self.datacollecter = DataCollector()
-        if self.interactions is None:
-            self.interactions = {}
-        if self.features is None:
-            self.features = {}
+        if self.feature_interactions is None:
             self.current_feature_id = 0
+            self.feature_interactions = nx.DiGraph()
             for i in range(num_env_features):
                 self.create_feature(env = True)
             for i in range(num_agent_features):
                 self.create_feature()
         else:
-            self.current_feature_id = len(self.features)
+            self.current_feature_id = len(self.feature_interactions.number_of_nodes())
         for i in range(initial_agents):
             agent = self.create_agent(num_traits = num_agent_traits)
             self.schedule.add(agent)
@@ -50,37 +47,32 @@ class World(Model):
         self.current_feature_id += 1
         return self.current_feature_id
 
-    def get_target_feature(self) -> Feature:
-        features = self.features.values()
-        target = self.random.choice(features)
-        return target
+    def get_features_list(self, env: bool = False) -> List[Feature]:
+        return [f for f in self.feature_interactions.nodes if f.env is env]
 
     def create_interaction(
         self,
         initiator: Feature,
     ) -> None:
-        initiator = initiator
-        extant_targets = [
-            x.target
-            for x
-            in self.interactions.values()
-            if x.initiator == initiator
-        ]
+        extant_targets = self.feature_interactions.neighbors(initiator)
         target_choices = [
             x
             for x
-            in self.features.values()
+            in self.feature_interactions.nodes
             if x not in extant_targets
         ]
         if len(target_choices) > 0:
             target = self.random.choice(target_choices)
-            key = (initiator.name, target.name)
             interaction = Interaction(
                 model = self,
                 initiator = initiator,
                 target = target,
             )
-            self.interactions[key] = interaction
+            self.feature_interactions.add_edge(
+                interaction.initiator,
+                interaction.target,
+                object = interaction
+            )
         return
 
     def create_feature(self,
@@ -89,14 +81,14 @@ class World(Model):
     ) -> Feature:
         feature_id = self.next_feature_id()
         feature = Feature(
-            model = self,
             feature_id = feature_id, 
             num_values = num_values, 
             env = env
         )
-        self.features[feature_id] = feature
+        self.feature_interactions.add_node(feature)
         if feature.env is False:
-            for i in range(self.num_feature_interactions):
+            num_ints = self.random.randrange(1, self.num_feature_interactions)
+            for i in range(num_ints):
                 self.create_interaction(feature)
         return feature
 
@@ -108,7 +100,7 @@ class World(Model):
         if utils is None:
             utils = self.base_utils
         traits = {}
-        agent_features = [f for f in self.features.values() if f.env is False]
+        agent_features = self.get_features_list(env=False)
         features = self.random.sample(agent_features, num_traits)
         for feature in features:
             value = self.random.choice(feature.values)
@@ -121,7 +113,41 @@ class World(Model):
         )
         return agent
 
+    def draw_feature_interactions(self):
+        g = self.feature_interactions
+        pos = nx.circular_layout(g)
+        labels = {n: n.name for n in pos.keys()}
+        nx.draw_networkx_nodes(g, pos, nodelist=self.get_features_list(env=False), node_color="tab:blue")
+        nx.draw_networkx_nodes(g, pos, nodelist=self.get_features_list(env=True), node_color="tab:green")
+        nx.draw_networkx_edges(g, pos)
+        nx.draw_networkx_labels(g, pos, labels)
+        plt.show()
+
+    def draw_utility_hist(self):
+        agent_fitness = [a.utils for a in self.schedule.agents]
+        plt.hist(agent_fitness)
+        plt.show()
+
+    def draw_features_hist(self):
+        num_features = [len(a.traits) for a in self.schedule.agents]
+        plt.hist(num_features)
+        plt.show()
+
+    def draw_age_hist(self):
+        ages = [a.age for a in self.schedule.agents]
+        plt.hist(ages)
+        plt.show()
+
+    def feature_distribution(self):
+        for feature in self.get_features_list():
+            agents = [a for a in self.schedule.agents if feature in a.traits]
+            traits = {v: len([a for a in agents if a.traits[feature] == v]) for v in feature.values}
+            print(feature, len(agents))
+            print(traits)
+
     def step(self):
         self.schedule.step()
+        print("Step{0} - {1} agents".format(self.schedule.time,len(self.schedule.agents)))
         self.datacollecter.collect(self)
+
 
